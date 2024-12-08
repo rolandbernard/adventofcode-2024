3a7e6341813f9b04da11f21dbb0d2c9e841706d2
Roland Bernard <rolli.bernard@gmail.com>
2024-12-08 16:42:20 +0100

    Cleaned up day 8 solutions

--- 08.resonant-collinearity/part1.rs
+++ 08.resonant-collinearity/part1.rs
@@ -1,34 +1,6 @@
+mod common;
+
 fn main() {
-    let map: Vec<Vec<char>> = std::io::stdin()
-        .lines()
-        .map(|line| line.unwrap().chars().collect())
-        .collect();
-    let mut antenna = Vec::new();
-    for (i, row) in map.iter().enumerate() {
-        for (j, &cell) in row.iter().enumerate() {
-            if cell != '.' {
-                antenna.push((i as i64, j as i64, cell));
-            }
-        }
-    }
-    let mut result = 0;
-    for i in 0..map.len() as i64 {
-        for j in 0..map[i as usize].len() as i64 {
-            let mut has_antinode = false;
-            for &(ai, aj, f) in &antenna {
-                let ni = 2 * ai - i;
-                let nj = 2 * aj - j;
-                if (i != ai || j != aj)
-                    && map.get(ni as usize).and_then(|r| r.get(nj as usize)) == Some(&f)
-                {
-                    has_antinode = true;
-                    break;
-                }
-            }
-            if has_antinode {
-                result += 1;
-            }
-        }
-    }
-    println!("Result: {}", result);
+    let map = common::parse_input();
+    println!("Result: {}", common::count_antinodes(&map, 1..=1));
 }
--- 08.resonant-collinearity/part2.rs
+++ 08.resonant-collinearity/part2.rs
@@ -1,36 +1,6 @@
-use std::collections::HashMap;
+mod common;
 
 fn main() {
-    let map: Vec<Vec<char>> = std::io::stdin()
-        .lines()
-        .map(|line| line.unwrap().chars().collect())
-        .collect();
-    let mut antennas = HashMap::new();
-    for (i, row) in map.iter().enumerate() {
-        for (j, &cell) in row.iter().enumerate() {
-            if cell != '.' {
-                antennas
-                    .entry(cell)
-                    .or_insert(Vec::new())
-                    .push((i as i64, j as i64));
-            }
-        }
-    }
-    let mut antinodes = vec![vec![0; map[0].len()]; map.len()];
-    for antenna in antennas.values() {
-        for (i1, j1) in antenna {
-            for (i2, j2) in antenna {
-                if i1 != i2 || j1 != j2 {
-                    let mut ni = *i2;
-                    let mut nj = *j2;
-                    while ni >= 0 && nj >= 0 && ni < map.len() as i64 && nj < map[0].len() as i64 {
-                        antinodes[ni as usize][nj as usize] = 1;
-                        ni += i2 - i1;
-                        nj += j2 - j1;
-                    }
-                }
-            }
-        }
-    }
-    println!("Result: {}", antinodes.into_iter().flatten().sum::<i64>());
+    let map = common::parse_input();
+    println!("Result: {}", common::count_antinodes(&map, 0..));
 }
