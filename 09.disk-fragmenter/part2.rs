c40a13bcba58cad86dd998b59fbe21e1b0153de7
Roland Bernard <rolli.bernard@gmail.com>
2024-12-09 18:39:57 +0100

    Cleaned up day 9 solution

--- 09.disk-fragmenter/part2.rs
+++ 09.disk-fragmenter/part2.rs
@@ -3,37 +3,32 @@ use std::io::Read;
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
-    let mut map = Vec::new();
+    let mut free = Vec::new();
     let mut files = Vec::new();
     let mut pos = 0;
     for (i, c) in input.chars().filter(|c| c.is_ascii_digit()).enumerate() {
         let size = c.to_digit(10).unwrap() as usize;
-        for _ in 0..c.to_digit(10).unwrap() {
-            if i % 2 == 0 {
-                map.push((i / 2) as i64);
-            } else {
-                map.push(-1);
-            }
+        if i % 2 == 0 {
+            files.push((pos, size));
+        } else {
+            free.push((pos, size));
         }
-        files.push((pos, size));
         pos += size;
     }
-    for (pos, size) in files.into_iter().rev() {
-        for i in 0..pos {
-            if map[i..i + size].iter().all(|&x| x == -1) {
-                for j in 0..size {
-                    map[i + j] = map[pos + j];
-                    map[pos + j] = -1;
-                }
+    let mut result = 0;
+    let mut heads = [0; 10];
+    for (id, (mut pos, size)) in files.into_iter().enumerate().rev() {
+        while heads[size] < free.len() && free[heads[size]].0 < pos {
+            if free[heads[size]].1 >= size {
+                pos = free[heads[size]].0;
+                free[heads[size]].0 += size;
+                free[heads[size]].1 -= size;
                 break;
+            } else {
+                heads[size] += 1;
             }
         }
-    }
-    let mut result = 0;
-    for (i, v) in map.into_iter().enumerate() {
-        if v != -1 {
-            result += i * v as usize;
-        }
+        result += id * size * (2 * pos + size - 1) / 2;
     }
     println!("Result: {}", result);
 }
